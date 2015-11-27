ef38b5cb6a1a65b9353f2dac5bdfd3fbf9c3481d
fugitiveinkc <blacksg1909@gmail.com>
2015-11-27 16:34:58 -0600

    Added a few ideas to generator

---
+++ Bebop_Solo_Generator.py
@@ -0,0 +1,34 @@
+'''
+
+
+Project: A Bebop solo generator
+
+Purpose: Given basic conditions of a musical composition (chords, time, etc), this program should be able to generate a decent bebop solo.
+
+Notes:
+
+
+'''
+
+
+'''
+
+
+**PSEUDOCODE**
+
+class Bebop_Generator:
+	
+	def __init__(self, **read in arguments for musical composition through cmd**):
+		**intial conditions for bebop solo generation**
+
+	def generate(self):
+		**Ideas on how to generate**
+		1) Create a rule set and go through each beat.
+		2) Make some decisions statisically more probable.
+		
+	def notate(self):
+		**Notate generated solo onto sheet music**
+
+
+'''
+
